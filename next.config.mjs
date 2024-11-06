f75a978024e0d30302e3c20eff66abfd68986846
cmj10301 <cmj10301@gmail.com>
2024-11-06 18:35:28 +0900

    Quill 백업

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: false,  // 여기에서 Strict Mode를 비활성화합니다.
+  };
+  
+  export default nextConfig;
+  
