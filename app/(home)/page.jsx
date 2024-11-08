795cdddf48ae785466c1bf452a0ddd3d195373f2
cmj10301 <cmj10301@gmail.com>
2024-11-08 18:39:31 +0900

    글 목록 로딩하는 동안 Spinner가 돌아감

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import { useState, useEffect } from "react";
-import { Container, ListGroup, ListGroupItem, Pagination } from "react-bootstrap";
+import { useState, useEffect, Suspense } from "react";
+import { ListGroup, ListGroupItem, Pagination, Spinner } from "react-bootstrap";
 
 export const dynamic = 'force-dynamic';
 
@@ -10,6 +10,7 @@ export default function Home() {
     const [posts, setPosts] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
+    const [loading, setloading] = useState(true);
     const limit = 10; // 한 페이지당 게시물 수
 
     useEffect(() => {
@@ -18,6 +19,7 @@ export default function Home() {
             const result = await response.json();
             setPosts(result.data);
             setTotalPages(result.totalPages);
+            setloading(false);
         }
         fetchPosts();
     }, [currentPage]);
@@ -29,14 +31,20 @@ export default function Home() {
     return (
         <div>
             <h2>글 목록</h2>
-            <ListGroup className="mx-5">
-                {posts.map((a) => (
-                    <ListGroupItem key={a._id} action href={`/detail/${a._id}`}>
-                        {a.제목}
-                        <span>👍 : {a.like || 0}</span>
-                    </ListGroupItem>
-                ))}
-            </ListGroup>
+            {loading ? (
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            ) : (
+                <ListGroup className="mx-5">
+                    {posts.map((a) => (
+                        <ListGroupItem key={a._id} action href={`/detail/${a._id}`}>
+                            {a.제목}
+                            <span>👍 : {a.like || 0}</span>
+                        </ListGroupItem>
+                    ))}
+                </ListGroup>
+            )}
 
             {/* 페이징 버튼 */}
             <Pagination className="mt-3 d-flex justify-content-center">
